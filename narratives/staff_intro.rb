12c703cf1c7b538f5a0c9b61aed93e965e079bd5
Nick Sahler <nick@sahler.co>
2016-09-21 09:53:00 -0400

    Give discobot some favorite things

--- narratives/staff_intro.rb
+++ narratives/staff_intro.rb
@@ -41,7 +41,7 @@ DiscourseEvent.on(:group_user_created) do | group_user |
 end
 
 DiscourseEvent.on(:post_created) do | post |
-  Jobs.enqueue_in( rand(1..3).seconds, :narrative_input,
+  Jobs.enqueue_in( rand(1..2).seconds, :narrative_input,
     user_id: post.user.id,
     post_id: post.id,
     narrative: 'staff_introduction',
@@ -78,7 +78,7 @@ Reply to this post and I'll quote you!},
 [/quote]
 
 Notice that new replies appear automatically, there's no need to refresh the page to get new information posted by other users.
-Next, let's create a new post. Create a topic in the `staff` category and mention me (@#{get_user.username}).},
+Next, let's create a new post. Create a topic in the `staff` category and mention a subject I like, like **unicorns** or **bacon** or **ninjas** or **monkeys**!!!.},
         topic_id: data[:topic_id]
       )
       
@@ -87,47 +87,21 @@ Next, let's create a new post. Create a topic in the `staff` category and mentio
     end
   end
 
-  EXAMPLES = ["https://meta.discourse.org/t/congratulations-most-stars-in-2013-github-octoverse/12483", "http://en.wikipedia.org/wiki/Ruby_on_Rails", "http://www.amazon.com/Apple-MacBook-MGX72LL-13-3-Inch-Display/dp/B0096VDM8G", "https://twitter.com/discourse/status/500399710377484288", "https://itunes.apple.com/us/app/duke-nukem-manhattan-project/id663811684?mt=8", "https://soundcloud.com/neilcic/mouthsilence", "http://stackoverflow.com/questions/25427024/what-is-the-used-for-in-ruby", "https://github.com/discourse/discourse/pull/2561", "https://github.com/rack/rack/blob/master/lib/rack/etag.rb", "http://www.flickr.com/photos/eho/149282456/", "http://imgur.com/gallery/1PGTI", "http://thenextweb.com/au/2012/11/18/kim-dotcoms-plan-to-give-new-zealanders-free-internet-could-just-work/?fromcat=au", "http://www.youtube.com/watch?v=9bZkp7q19f0", "http://vimeo.com/channels/staffpicks/97765630", "http://www.funnyordie.com/videos/05c8ec50ed/between-two-ferns-with-zach-galifianakis-richard-branson", "http://techcrunch.com/2013/02/05/jeff-atwood-launches-discourse/"]
-  wrap = proc { |a| "- `#{a}`\n"}
+  EXAMPLES = {
+    "unicorn" => "Did you know that the unicorn is Scotland's national animal? :unicorn: \nhttps://en.wikipedia.org/wiki/Unicorn",
+    "ninja" => "Did you know that ninjas used to hide in the same spot for days, disguised as inanimate objects like rocks and trees :leaves:? \nhttp://nerdreactor.com/wp-content/uploads/2012/12/Ninja_Gaiden_NES_02.jpg",
+    "bacon" => ":pig: :pig: :pig: :pig: :pig: :pig: \nhttps://media.giphy.com/media/10l8MVei2OxbuU/giphy.gif \nhttps://media.giphy.com/media/qZiUOutzxgfKM/giphy.gif",
+    "monkey" => ":monkey: :fries: \nhttps://www.youtube.com/watch?v=FjqfX8-L0Tk"
+  }
 
   state :waiting_user_newtopic, on: 'reply' do | user, post |
-    # Better way to do this other than PostAlerter? 
-    if data[:topic_id] != post.topic.id && post.raw.include?("@#{get_user.username}")
-      data[:topic_id] = post.topic.id
-      dirty
-
+    if post.topic.category.slug === 'staff' && (subject = /(unicorn(s))|(bacon)|(ninja(s))|(monkey(s))/i.match(post.raw))
       PostCreator.create(
         get_user,
-        raw: %Q{Great! Next let's have a conversation.
-One great feature of Discourse is called OneBoxing. OneBoxing is when content from links is expanded and a useful summary is displayed, like this:
-https://en.wikipedia.org/wiki/Cat
-
-Reply here with some links you find interesting to check out OneBoxing!
-If you need some ideas, feel free to ask for `help`!},
-        topic_id: data[:topic_id])
-
-      :duel
+        raw: "Omg, I love #{ subject.to_s }!!! \n #{EXAMPLES[subject.to_s.downcase.singularize]}",
+        topic_id: post.topic.id
+      )
     end
   end
 
-  state :duel, on: 'reply' do | user, post |
-    return if data[:topic_id] != post.topic.id
-
-    post.post_analyzer.cook post.raw, {}
-
-    if post.post_analyzer.found_oneboxes?
-      PostCreator.create(get_user, raw: "Nice onebox thing! Here's something I found: \n#{ EXAMPLES.sample }", topic_id: data[:topic_id])
-    else
-      PostCreator.create(get_user, raw: "That does not have a onebox in it! If you need some inspiration, here are some links you can paste: \n\n#{ EXAMPLES.map(&wrap).join('') }", topic_id: data[:topic_id])
-    end
-  end
-
-  state :waiting_user_quoted, on: 'reply' do | user, post |
-    if data[:topic_id] === post.topic.id && /\[.*quote\s*=\s*"discoursebot/.match(post.raw)
-      PostCreator.create( get_user, raw: (I18n.t 'narratives.user_quoted'), topic_id: data[:topic_id])
-      :waiting_image
-    else
-      PostCreator.create( get_user, raw: (I18n.t 'narratives.user_quoted_fallback', username: post.user.username ), topic_id: data[:topic_id])
-    end
-  end
 end
