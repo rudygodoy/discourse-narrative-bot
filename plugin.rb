16cb7b9de428eaf834b5bc0c1f5858996086ebfa
Nick Sahler <nick@sahler.co>
2016-10-05 17:17:37 -0400

    Broken status endpoint

--- plugin.rb
+++ plugin.rb
@@ -18,6 +18,7 @@ after_initialize do
 
   load File.expand_path("../app/models/group_user.rb", __FILE__)
   load File.expand_path("../narrative.rb", __FILE__)
+  # TODO Automatically load this folder in 
   load File.expand_path("../narratives/staff_intro.rb", __FILE__)
 
   module ::Narratives
@@ -29,13 +30,19 @@ after_initialize do
     class NarrativesController < ::ApplicationController
       def reset
         ::PluginStore.set(PLUGIN_NAME, "narrative_#{params[:narrative]}_#{params[:user_id]}", nil) 
-        render :json, {}
+        render :json, {}.to_s
+      end
+
+      def status
+        render :json, ::PluginStore.get(PLUGIN_NAME, "narrative_#{params[:narrative]}_#{params[:user_id]}")
       end
     end
   end
 
   Narratives::Engine.routes.draw do
     get "/reset/:user_id/:narrative" => "narratives#reset", constraints: AdminConstraint.new
+    get "/status/:user_id/:narrative" => "narratives#status", constraints: AdminConstraint.new
+
   end
 
   Discourse::Application.routes.prepend do
