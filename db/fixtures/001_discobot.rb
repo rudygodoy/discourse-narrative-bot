621ccded1693b0176c292067801d3fcd1abbb47a
Guo Xiang Tan <tgx_world@hotmail.com>
2017-03-10 14:10:31 +0800

    FIX: Seed file should not override bot's attribute once it has been created.

--- db/fixtures/001_discobot.rb
+++ db/fixtures/001_discobot.rb
@@ -1,42 +1,42 @@
 discobot_username ='discobot'
-user = User.find_by("id <> -2 and username_lower = '#{discobot_username}'")
+user = User.find_by(id: -2)
 
-if user
-  user.update_attributes!(username: UserNameSuggester.suggest(discobot_username))
-end
+if !user
+  suggested_username = UserNameSuggester.suggest(discobot_username)
 
-User.seed do |u|
-  u.id = -2
-  u.name = discobot_username
-  u.username = discobot_username
-  u.username_lower = discobot_username
-  u.email = "discobot_email"
-  u.password = SecureRandom.hex
-  u.active = true
-  u.admin = true
-  u.moderator = true
-  u.approved = true
-  u.trust_level = TrustLevel[4]
+  User.seed do |u|
+    u.id = -2
+    u.name = discobot_username
+    u.username = suggested_username
+    u.username_lower = suggested_username.downcase
+    u.email = "discobot_email"
+    u.password = SecureRandom.hex
+    u.active = true
+    u.admin = true
+    u.moderator = true
+    u.approved = true
+    u.trust_level = TrustLevel[4]
+  end
+
+  # TODO Pull the user avatar from that thread for now. In the future, pull it from a local file or from some central discobot repo.
+  UserAvatar.import_url_for_user(
+    "https://cdn.discourse.org/dev/uploads/default/original/2X/e/edb63d57a720838a7ce6a68f02ba4618787f2299.png",
+    User.find(-2),
+    override_gravatar: true
+  )
 end
 
 bot = User.find(-2)
 
-bot.user_option.update_attributes!(
+bot.user_option.update!(
   email_private_messages: false,
   email_direct: false
 )
 
 if !bot.user_profile.bio_raw
-  bot.user_profile.update_attributes!(
+  bot.user_profile.update!(
     bio_raw: I18n.t('discourse_narrative_bot.bio', site_title: SiteSetting.title, discobot_username: bot.username)
   )
 end
 
 Group.user_trust_level_change!(-2, TrustLevel[4])
-
-# TODO Pull the user avatar from that thread for now. In the future, pull it from a local file or from some central discobot repo.
-UserAvatar.import_url_for_user(
-  "https://cdn.discourse.org/dev/uploads/default/original/2X/e/edb63d57a720838a7ce6a68f02ba4618787f2299.png",
-  User.find_by(username: discobot_username),
-  override_gravatar: true
-)
