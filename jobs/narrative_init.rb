47d0e125596f504acac5b30b0b77275f7e690665
Guo Xiang Tan <tgx_world@hotmail.com>
2017-04-12 11:58:01 +0800

    FIX: Initiate bot job should be in critical sidekiq queue.

--- jobs/narrative_init.rb
+++ jobs/narrative_init.rb
@@ -1,5 +1,7 @@
 module Jobs
   class NarrativeInit < Jobs::Base
+    sidekiq_options queue: 'critical'
+
     def execute(args)
       if user = User.find_by(id: args[:user_id])
         args[:klass].constantize.new.input(:init, user)
