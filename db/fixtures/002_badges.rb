c8494e9e1190b4202dc75c61298261205dc878a5
Guo Xiang Tan <tgx_world@hotmail.com>
2017-05-10 00:08:33 +0800

    Move bot badges into the right badge grouping.

--- db/fixtures/002_badges.rb
+++ db/fixtures/002_badges.rb
@@ -6,18 +6,26 @@ Badge
   .where(name: 'Complete Discobot Advanced User Track')
   .update_all(name: DiscourseNarrativeBot::AdvancedUserNarrative::BADGE_NAME)
 
-unless Badge.find_by(name: DiscourseNarrativeBot::NewUserNarrative::BADGE_NAME)
-  Badge.create!(
+new_user_narrative_badge = Badge.find_by(name: DiscourseNarrativeBot::NewUserNarrative::BADGE_NAME)
+
+unless new_user_narrative_badge
+  new_user_narrative_badge = Badge.create!(
     name: DiscourseNarrativeBot::NewUserNarrative::BADGE_NAME,
     description: "Completed Discourse narrative bot's new user track",
     badge_type_id: 3
   )
 end
 
-unless Badge.find_by(name: DiscourseNarrativeBot::AdvancedUserNarrative::BADGE_NAME)
-  Badge.create!(
+advanced_user_narrative_badge = Badge.find_by(name: DiscourseNarrativeBot::AdvancedUserNarrative::BADGE_NAME)
+
+unless advanced_user_narrative_badge
+  advanced_user_narrative_badge = Badge.create!(
     name: DiscourseNarrativeBot::AdvancedUserNarrative::BADGE_NAME,
     description: "Completed Discourse narrative bot's advanced user track",
     badge_type_id: 2
   )
+end
+
+[new_user_narrative_badge, advanced_user_narrative_badge].each do |badge|
+  badge.update!(badge_grouping: BadgeGrouping.find(1))
 end
