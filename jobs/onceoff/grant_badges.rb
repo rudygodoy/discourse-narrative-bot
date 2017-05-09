0725ff406821fdcc46656a62c33d3e6f6cafde2d
Guo Xiang Tan <tgx_world@hotmail.com>
2017-05-10 00:15:49 +0800

    Fix uninitialized constant error.

--- jobs/onceoff/grant_badges.rb
+++ jobs/onceoff/grant_badges.rb
@@ -3,15 +3,15 @@ module Jobs
     class GrantBadges < ::Jobs::Onceoff
       def execute_onceoff(args)
         new_user_track_badge = Badge.find_by(
-          name: DiscourseNarrativeBot::NewUserNarrative::BADGE_NAME
+          name: ::DiscourseNarrativeBot::NewUserNarrative::BADGE_NAME
         )
 
         advanced_user_track_badge = Badge.find_by(
-          name: DiscourseNarrativeBot::AdvancedUserNarrative::BADGE_NAME
+          name: ::DiscourseNarrativeBot::AdvancedUserNarrative::BADGE_NAME
         )
 
         PluginStoreRow.where(
-          plugin_name: DiscourseNarrativeBot::PLUGIN_NAME,
+          plugin_name: ::DiscourseNarrativeBot::PLUGIN_NAME,
           type_name: 'JSON'
         ).find_each do |row|
 
@@ -20,11 +20,11 @@ module Jobs
           user = User.find_by(id: row.key)
 
           if user && completed
-            if completed.include?(DiscourseNarrativeBot::NewUserNarrative.to_s)
+            if completed.include?(::DiscourseNarrativeBot::NewUserNarrative.to_s)
               BadgeGranter.grant(new_user_track_badge, user)
             end
 
-            if completed.include?(DiscourseNarrativeBot::AdvancedUserNarrative.to_s)
+            if completed.include?(::DiscourseNarrativeBot::AdvancedUserNarrative.to_s)
               BadgeGranter.grant(advanced_user_track_badge, user)
             end
           end
